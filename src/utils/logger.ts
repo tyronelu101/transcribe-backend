ae498f6a3c681c7efeed11d6989cd722083f07d7
tyrone lu <tyrone@DESKTOP-TY.localdomain>
2022-12-16 15:18:29 -0600

    Update logger to not display log in test mode

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,13 @@
 const info = (...params: string[]) => {
-    console.log(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params)
+    }
 }
 
 const error = (...params: string[]) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params)
+    }
 }
 
 module.exports = {
